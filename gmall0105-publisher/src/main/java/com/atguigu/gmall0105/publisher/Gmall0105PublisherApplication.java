a9b0c314db1f6ce0c8972b0579538d5f9ea01cef
zhangchen <windyzj@163.com>
2020-06-20 19:18:06 +0800

    数据查询接口

---
+++ gmall0105-publisher/src/main/java/com/atguigu/gmall0105/publisher/Gmall0105PublisherApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall0105.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0105PublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0105PublisherApplication.class, args);
+    }
+
+}
